075bb8adfd81cb656175bb82c4a131c79005217d
Ariell Dattage <ariell.dattage@Ariells-MBP.localdomain>
2021-11-17 16:40:47 -0700

    public

--- server/index.js
+++ server/index.js
@@ -4,10 +4,11 @@ const path = require("path")
 const app = express()
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, "../index.html"))
+    res.sendFile(path.join(__dirname, "../public"))
 })
 
 app.use('/css', express.static(path.join(__dirname, "../styles.css")))
+// app.use('/js', express.static(path.join(__dirname, "..//")))
 
 const port = process.env.PORT || 4005
 
